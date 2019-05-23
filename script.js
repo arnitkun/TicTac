aa32fc7b252525be4c512dda2bf1d5c8b8f560ea
unknown <ankit.nitagartala@gmail.com>
2019-05-23 15:44:37 +0530

    added winning conditions

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ function nextMove(square) {
 }
 
 function switchTurn() {
-  if (document.turn == "X") {
+		
+  if (checkForWinner(document.turn)) {
+    setMessage("Congratulations, " + document.turn + "!  You win!");
+  } else if (document.turn == "X") {
     document.turn = "O";
+    setMessage("It's " + document.turn + "'s turn!");
   } else {
     document.turn = "X";
+    setMessage("It's " + document.turn + "'s turn!");
   }
-
-  setMessage("It's " + document.turn + "'s turn!");
 }
 
 function nextMove(square) {
@@ -30,4 +33,32 @@ function nextMove(square) {
   } else {
     setMessage("That square is already used.");
   }
+}
+
+function getCell(number){
+  return document.getElementById("s" + number).innerText;
+}
+
+function checkRow(a, b, c, move) {
+  var result = false;
+  if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
+    result = true;
+  }
+  return result;
+}
+
+function checkForWinner(move) {
+  var result = false;
+  if (checkRow(1, 2, 3, move) || 
+    checkRow(4, 5, 6, move) ||
+    checkRow(7, 8, 9, move) || 
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(3, 6, 9, move) ||
+    checkRow(1, 5, 9, move) ||
+    checkRow(3, 5, 7, move)) {
+    
+    result = true;
+  }
+  return result;
 }
