4a4007007e18a85936fbec220b1654b82fe735c1
arnitkun <ankit.nitagartala@gmail.com>
2019-10-30 15:53:51 +0530

    enhancement: added extra request endpoints.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('hbs');//handlebars, why wasn't this told to students? Perfect example why academia is useless
 const fs = require('fs');
 
 var app = express();
@@ -8,6 +8,6 @@ hbs.registerPartials(__dirname + '/views/partials'); //setting up for using hand
 app.set('view engine', 'hbs');//setting the view engine as hbs. But what is the view engine?
 app.use(express.static(__dirname + '/public')); //static content serving,using middleware
 
-app.use('/maintenance', (req, res, next) => {
+app.use('/maintenance', (req, res, next) => {//serves up the maintenance page
     res.render('maintenance.hbs');
 });
